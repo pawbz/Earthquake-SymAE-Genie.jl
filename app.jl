2828b5c9492f64e7f6e3e4f3136224c1e4075c4c
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2024-12-02 12:22:22 +0530

    improved efficiency and maintainability

--- app.jl
+++ app.jl
@@ -2,11 +2,11 @@
 using GenieFramework, JLD2, DataFrames, CSV, StatsBase, PlotlyBase, DSP, ColorSchemes, Healpix, Random, Colors
 @genietools
 
-usvs_color_scheme = ColorSchemes.darkrainbow
-max_pixels = 60
+const _usvs_color_scheme = ColorSchemes.darkrainbow
+const _max_pixels = 60
 
-# const _model_filename = "20240912T190943289"
-const _model_filename = "20241124T150609280"
+const _model_filename = "20240912T190943289"
+# const _model_filename = "20241124T150609280"
 const _available_eqs_jld2 = filter(x -> occursin(".jld2", x), readdir(joinpath("data", _model_filename), join=true))
 
 # function load_eq_lat_long(available_eqs_jld2)
@@ -41,7 +41,6 @@ function jld2_to_eqname(jld2file)
 end
 
 envelope(x) = abs.(hilbert(x))
-
 # const first_available_eq_name = available_eqs_name[1]
 # default_eq = available_eqs_name[1]
 
@@ -50,8 +49,8 @@ envelope(x) = abs.(hilbert(x))
 # columns = names(df)
 # the soil_data table contains the location of each station along with other information
 # soil_data = CSV.read("data/soil_data.csv", DataFrame)
-tgrid = collect(1:256)
-stf = range(0.0, stop=1.0, length=256)
+const _tgrid = range(-200, stop=200, length=400)[101:356]
+# stf = range(0.0, stop=1.0, length=256)
 
 function read_all_JLD2_files(available_eqs_jld2)
     eqs = []
@@ -86,15 +85,123 @@ function get_pixels(jld2fileindex, eq_data)
     pixels = filter(x -> tryparse(Int, x) !== nothing, collect(k))
     return pixels
 end
-function get_stf_bundle(jld2fileindex, eq_data, selected_pixels)
-    eqfile = eq_data[jld2fileindex]
-    stf = eqfile["$(selected_pixels)"]
-    return stf
+
+function get_stf_traces(selected_eq, selected_pixel)
+    jld_file_index = findall(x -> occursin(selected_eq, x), _available_eqs_jld2)[1]
+        stf_bundle = _eq_data[jld_file_index]["$(string(selected_pixel))"]
+        stf = (envelope(stf_bundle["USVS"][:, get_min_KL_instance_id(jld_file_index, _eq_data)]))
+        stf_std = dropdims(std(stf_bundle["USVS"], dims=2), dims=(2,3))
+        stf_upper = stf .+ stf_std
+        stf_lower = stf .- stf_std
+
+        raw_env_mean = (stf_bundle["RAW"])
+        raw_env_upper = (stf_bundle["RAW_upper_bound"])
+        raw_env_lower = (stf_bundle["RAW_lower_bound"])
+
+        # traces[!] = [] # With the [!] suffix we reassign the array without triggering a UI update
+        traces = [
+            scatter(
+                x=_tgrid,
+                y=stf,
+                line=attr(color="rgb(0,0,128)"),
+                name="Source Time Function Using Variational SymAE",
+                mode="lines",
+                legendgroup="group1",
+            ),
+            scatter(
+                x=vcat(_tgrid, reverse(_tgrid)), # x, then x reversed
+                y=vcat(stf_upper, reverse(stf_lower)), # upper, then lower reversed
+                fill="toself",
+                fillcolor="rgba(0,0,128,0.2)",
+                line=attr(color="rgba(0,0,0,0)"),
+                hoverinfo="skip",
+                legendgroup="group1",
+                showlegend=false
+            ),
+            scatter(x=_tgrid,
+                y=raw_env_mean,
+                line=attr(color="rgb(139,0,0)"),
+                legendgroup="group2",
+                name="Raw Displacement Seismogram Envelope",
+                mode="lines"),
+            scatter(
+                x=vcat(_tgrid, reverse(_tgrid)), # x, then x reversed
+                y=vcat(raw_env_upper, reverse(raw_env_lower)), # upper, then lower reversed
+                fill="toself",
+                fillcolor="rgba(139,0,0,0.2)",
+                line=attr(color="rgba(0,0,0,0)"),
+                legendgroup="group2",
+                hoverinfo="skip",
+                showlegend=false
+            )
+        ]
+        return traces
 end
 
 const _available_eqs = jld2_to_eqname.(_available_eqs_jld2)
 const _eq_data = read_all_JLD2_files(_available_eqs_jld2)
 const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
+const _stf_layout = PlotlyBase.Layout(
+    title=attr(
+        x=0.5,                   # Horizontal position (0 is left, 0.5 is center, 1 is right)
+        y=0.97,                   # Vertical position (0 is bottom, 1 is top)
+        font=attr(size=22)     # Customize font size if needed
+    ),
+    template="plotly_white",
+    height=700,
+    # width=900,
+    xaxis=attr(
+        title="Relative Time (s)",
+        titlefont=attr(size=22),
+        font=attr(size=22),
+        tickfont=attr(size=22),
+        nticks=10,
+        gridwidth=1,
+        range=(-50, 80),
+    ),
+    yaxis=attr(
+        title="Normalized Amplitude",
+        titlefont=attr(size=22),
+        font=attr(size=22),
+        tickfont=attr(size=22),
+        nticks=10,
+        gridwidth=1,
+        range=(0, 10),
+    ),
+    legend=attr(
+        orientation="h",         # Horizontal legend
+        y=-0.2,                  # Position below x-axis (adjust this value as needed)
+        font=attr(size=22),
+        x=0.5,                   # Center the legend horizontally
+        xanchor="center",        # Anchor the legend to the center of the x position
+        yanchor="top",           # Align to the top so that it moves downward from x-axis
+        traceorder="grouped",    # Group legend items to display on multiple rows
+    )
+)
+const _usvs_layout = PlotlyBase.Layout(
+    template="plotly_white",
+    height=1200,
+    title="Raw Envelope Stacking Vs. SymAE Source Time Functions",
+    yaxis_anchor="x",
+    legend=attr(title="pixel (colatitude, longitude)"),
+    yaxis=attr(showgrid=false, showticklabels=false),
+    xaxis=attr(
+        titlefont=attr(size=22),
+        font=attr(size=22),
+        tickfont=attr(size=22),
+        range=(-50, 60),
+        domain=[0, 0.48],
+        showgrid=true,
+        title="Relative Time (s)"),
+    xaxis2=attr(
+        titlefont=attr(size=22),
+        font=attr(size=22),
+        tickfont=attr(size=22),
+        range=(-50, 60),
+        showgrid=true,
+        domain=[0.52, 1],
+        title="Relative Time (s)"),
+)
 @app begin
     @out available_eqs = _available_eqs
     @out eq_lat_selected = 0.0
@@ -108,14 +215,14 @@ const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
         eq_long_selected = _eq_loc_data[findfirst(x -> x == selected_eq, available_eqs)]["longitude"]
     end
 
-@out eq_location_traces = [PlotlyBase.scattermapbox(    
-                    lat=[eqloc["latitude"]],
-                    lon=[eqloc["longitude"]],
-                    mode="markers",
-                    size=10,
-                    marker=attr(size=10, color=eqloc["depth"], linecolor="black", colorscale="Blues", cmin=300, cmax=700),
-                    name="",
-                    hovertext=eqname) for (eqname, eqloc) in zip(_available_eqs, _eq_loc_data)]
+    @out eq_location_traces = [PlotlyBase.scattermapbox(
+        lat=[eqloc["latitude"]],
+        lon=[eqloc["longitude"]],
+        mode="markers",
+        size=10,
+        marker=attr(size=10, color=eqloc["depth"], linecolor="black", colorscale="Blues", cmin=300, cmax=700),
+        name="",
+        hovertext=eqname) for (eqname, eqloc) in zip(_available_eqs, _eq_loc_data)]
 
 
     @out tab_ids = ["tab_stf", "tab_usvs"]
@@ -123,7 +230,7 @@ const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
     @in selected_tab = "tab_stf"
 
 
-    @out tgrid = range(-200, stop=200, length=400)[101:356]
+
     @in selected_pixel = ""
     @onchange available_pixels begin
         selected_pixel = first(available_pixels)
@@ -132,94 +239,11 @@ const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
     # @out traces = [scatter(x=collect(1:10), y=randn(10)), scatter(x=collect(1:10), y=randn(10))]
     @out traces = [scatter()]
     @onchange selected_eq, selected_pixel begin
-        jld_file_index = findall(x -> occursin(selected_eq, x), _available_eqs_jld2)[1]
-        stf_bundle = get_stf_bundle(jld_file_index, _eq_data, selected_pixel)
-        stf = vec(envelope(stf_bundle["USVS"][:, get_min_KL_instance_id(jld_file_index, _eq_data)]))
-        stf_std = vec(std(stf_bundle["USVS"], dims=2))
-        stf_upper = stf .+ stf_std
-        stf_lower = stf .- stf_std
-
-        raw_env_mean = vec(stf_bundle["RAW"])
-        raw_env_upper = vec(stf_bundle["RAW_upper_bound"])
-        raw_env_lower = vec(stf_bundle["RAW_lower_bound"])
-
-        # traces[!] = [] # With the [!] suffix we reassign the array without triggering a UI update
-        traces = [
-            scatter(
-                x=tgrid,
-                y=stf,
-                line=attr(color="rgb(0,0,100)"),
-                name="Source Time Function Using Variational SymAE",
-                mode="lines",
-                legendgroup="group1",
-            ),
-            scatter(
-                x=vcat(tgrid, reverse(tgrid)), # x, then x reversed
-                y=vcat(stf_upper, reverse(stf_lower)), # upper, then lower reversed
-                fill="toself",
-                fillcolor="rgba(0,0,100,0.5)",
-                line=attr(color="rgba(0,0,0,0)"),
-                hoverinfo="skip",
-                legendgroup="group1",
-                showlegend=false
-            ),
-            scatter(x=tgrid,
-                y=raw_env_mean,
-                line=attr(color="rgb(100,0,0)"),
-                legendgroup="group2",
-                name="Raw Displacement Seismogram Envelope",
-                mode="lines"),
-            scatter(
-                x=vcat(tgrid, reverse(tgrid)), # x, then x reversed
-                y=vcat(raw_env_upper, reverse(raw_env_lower)), # upper, then lower reversed
-                fill="toself",
-                fillcolor="rgba(100,0,0,0.2)",
-                line=attr(color="rgba(0,0,0,0)"),
-                legendgroup="group2",
-                hoverinfo="skip",
-                showlegend=false
-            )
-        ]
-        # @push traces 
+        traces = get_stf_traces(selected_eq, selected_pixel) 
     end
 
-    @out stf_layout = PlotlyBase.Layout(
-        title=attr(
-            x=0.5,                   # Horizontal position (0 is left, 0.5 is center, 1 is right)
-            y=0.97,                   # Vertical position (0 is bottom, 1 is top)
-            font=attr(size=22)     # Customize font size if needed
-        ),
-        template="plotly_white",
-        height=700,
-        # width=900,
-        xaxis=attr(
-            title="Time (s)",
-            titlefont=attr(size=22),
-            font=attr(size=22),
-            tickfont=attr(size=22),
-            nticks=10,
-            gridwidth=1,
-            range=(-50, 80),
-        ),
-        yaxis=attr(
-            title="Normalized Amplitude",
-            titlefont=attr(size=22),
-            font=attr(size=22),
-            tickfont=attr(size=22),
-            nticks=10,
-            gridwidth=1,
-            range=(0, 8),
-        ),
-        legend=attr(
-            orientation="h",         # Horizontal legend
-            y=-0.2,                  # Position below x-axis (adjust this value as needed)
-            font=attr(size=22),
-            x=0.5,                   # Center the legend horizontally
-            xanchor="center",        # Anchor the legend to the center of the x position
-            yanchor="top",           # Align to the top so that it moves downward from x-axis
-            traceorder="grouped",    # Group legend items to display on multiple rows
-        )
-    )
+    @out usvs_layout = _usvs_layout
+    @out stf_layout = _stf_layout
 
 
     @in selected_rec_pixel = ""
@@ -237,7 +261,7 @@ const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
     end
 
     @out traces_usvs = [scatter()]
-    @onchange selected_eq, available_pixels, tgrid begin
+    @onchange selected_eq, available_pixels begin
 
         jld_file_index = findall(x -> occursin(selected_eq, x), _available_eqs_jld2)[1]
         all_pixels = get_pixels(jld_file_index, _eq_data)
@@ -252,9 +276,11 @@ const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
                 floor(Int, rad2deg(x))
             end
             jld_file_index = findall(x -> occursin(selected_eq, x), _available_eqs_jld2)[1]
-            stf_bundle = get_stf_bundle(jld_file_index, _eq_data, string(pixel))
-            pixel_stf = vec(mean(envelope(stf_bundle["USVS"]), dims=2))
-            pixel_raw = vec(stf_bundle["RAW"])
+            stf_bundle = _eq_data[jld_file_index]["$(string(pixel))"]
+
+            pixel_stf = envelope(stf_bundle["USVS"][:, get_min_KL_instance_id(jld_file_index, _eq_data)])
+            # pixel_stf = vec(mean(envelope(stf_bundle["USVS"]), dims=2))
+            pixel_raw = stf_bundle["RAW"]
 
             av = av .+ pixel_stf
             av_raw = av_raw .+ pixel_raw
@@ -263,14 +289,14 @@ const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
             pixel_stf = pixel_stf ./ maximum(pixel_stf) .* 2.5
             pixel_raw = pixel_raw ./ maximum(pixel_raw) .* 2.5
 
-            # @show get(usvs_color_scheme, ipixel/npixels), ipixel/npixels
+            # @show get(_usvs_color_scheme, ipixel/npixels), ipixel/npixels
             push!(traces_usvs,
                 scatter(
-                    x=tgrid,
+                    x=_tgrid,
                     y=pixel_stf .+ current_yoffset,
-                    # line = attr(color = "white"),#hex(get(usvs_color_scheme, ipixel/npixels))),
-                    fillcolor=hex(get(usvs_color_scheme, ipixel / npixels)),
-                    line=attr(color="black"),#hex(get(usvs_color_scheme, ipixel/npixels))),
+                    # line = attr(color = "white"),#hex(get(_usvs_color_scheme, ipixel/npixels))),
+                    fillcolor=hex(get(_usvs_color_scheme, ipixel / npixels)),
+                    line=attr(color="black"),#hex(get(_usvs_color_scheme, ipixel/npixels))),
                     fill="tonexty",
                     mode="lines",
                     name=string(pixel, ": ", angles),
@@ -280,11 +306,11 @@ const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
 
             push!(traces_usvs,
                 scatter(
-                    x=tgrid,
+                    x=_tgrid,
                     y=pixel_raw .+ current_yoffset,
-                    # line = attr(color = "white"),#hex(get(usvs_color_scheme, ipixel/npixels))),
-                    fillcolor=hex(get(usvs_color_scheme, ipixel / npixels)),
-                    line=attr(color="black"),#hex(get(usvs_color_scheme, ipixel/npixels))),
+                    # line = attr(color = "white"),#hex(get(_usvs_color_scheme, ipixel/npixels))),
+                    fillcolor=hex(get(_usvs_color_scheme, ipixel / npixels)),
+                    line=attr(color="black"),#hex(get(_usvs_color_scheme, ipixel/npixels))),
                     fill="tonexty",
                     mode="lines",
                     showlegend=false,
@@ -298,7 +324,7 @@ const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
         av_raw .= av_raw ./ maximum(av_raw) .* 2.5
         push!(traces_usvs,
             scatter(
-                x=tgrid,
+                x=_tgrid,
                 y=av .+ last_yoffset,
                 line=attr(color="black"),
                 mode="lines",
@@ -308,7 +334,7 @@ const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
             ))
         push!(traces_usvs,
             scatter(
-                x=tgrid,
+                x=_tgrid,
                 y=av_raw .+ last_yoffset,
                 line=attr(color="black"),
                 mode="lines",
@@ -321,29 +347,7 @@ const _eq_loc_data = read_all_loc_JLD2_files(_available_eqs_jld2)
     end
 
 
-    @out usvs_layout = PlotlyBase.Layout(
-        template="plotly_white",
-        height=1200,
-        title="Raw Envelope Stacking Vs. SymAE Source Time Functions",
-        yaxis_anchor="x",
-        yaxis=attr(showgrid=false, showticklabels=false),
-        xaxis=attr(
-            titlefont=attr(size=22),
-            font=attr(size=22),
-            tickfont=attr(size=22),
-            range=(-50, 60),
-            domain=[0, 0.48],
-            showgrid=true,
-            title="Time (s)"),
-        xaxis2=attr(
-            titlefont=attr(size=22),
-            font=attr(size=22),
-            tickfont=attr(size=22),
-            range=(-50, 60),
-            showgrid=true,
-            domain=[0.52, 1],
-            title="Time (s)"),
-    )
+
 
     @out mapcolor = []
     @in data_click = Dict{String,Any}()  # data from map click event
